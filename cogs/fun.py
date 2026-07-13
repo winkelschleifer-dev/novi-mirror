6d16e04c81940188f16965d7071d601f7255f3d9
winkelschleifer <winkelschleifer@noreply.codeberg.org>
2026-07-13 09:23:56 +0200

    Add cogs/fun.py
    Signed-off-by: winkelschleifer <winkelschleifer@noreply.codeberg.org>

---
+++ cogs/fun.py
@@ -0,0 +1,22 @@
+from discord.ext import commands
+import discord
+
+class Fun(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+
+  @commands.command()
+  async def howfemboy(self, ctx, member:discord.Member = None):
+    if member is None:
+      member = ctx.author
+
+    percent = member.id % 101
+
+    await ctx.send(f"{member.display_name} is {percent}% femboy")
+
+
+
+
+
+async def setup(bot):
+  await bot.add_cog(General(bot))
