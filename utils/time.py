47bdf9a8931c4c6be9b430df32752a948cd3a545
winkelschleifer <winkelschleifer@noreply.codeberg.org>
2026-07-13 11:32:41 +0200

    Add utils/time.py

---
+++ utils/time.py
@@ -0,0 +1,25 @@
+from datetime import timedelta
+
+def parse_time(time_string):
+  unit = time_string[-1]
+  value = time_string[:-1]
+
+  value = int(value)
+
+  if value > 0:
+
+      if unit == "s":
+        output = datetime.timedelta(seconds=value)
+      elif unit == "m":
+        output = datetime.timedelta(minutes=value)
+      elif unit == "h":
+        output = datetime.timedelta(hours=value)
+      elif unit == "d":
+        output = datetime.timedelta(days=value)
+      else:
+        output = "unitErrot"
+  else:
+    output = "valueError"
+
+
+  return output
