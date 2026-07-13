890222afc2a37f838ca1b7d53f4cdedb5b4a0c25
winkelschleifer <winkelschleifer@noreply.codeberg.org>
2026-07-13 09:42:10 +0200

    Update cogs/general.py

--- cogs/general.py
+++ cogs/general.py
@@ -2,15 +2,16 @@ from discord.ext import commands
 import discord
 
 class General(commands.Cog):
+  """General commands"""
   def __init__(self, bot):
     self.bot = bot
 
-  @commands.command()
+  @commands.command(help="Gets Novi's brain delay")
   async def ping(self, ctx):
     latency = round(self.bot.latency * 1000)
     await ctx.send(f"🏓 Pong! Latency: {latency} ms")
 
-  @commands.command()
+  @commands.command(help="Shows some info about the bot")
   async def about(self, ctx):
     embed = discord.Embed(
       title="Novi",
@@ -25,14 +26,14 @@ class General(commands.Cog):
 
     await ctx.send(embed=embed)
 
-  @commands.command()
-  async def help(self, ctx):
-    embed=discord.Embed(
-      title="Help",
-      description="My prefix is *n!*. Commands:\nhelp - displays this page\nabout - displays basic bot info\nping - displays bot brain delay",
-      color=0xfff600
-    )
-    await ctx.send(embed=embed)
+#  @commands.command()
+ # async def help(self, ctx):
+  #  embed=discord.Embed(
+      #title="Help",
+     # description="My prefix is *n!*. Commands:\nhelp - displays this page\nabout - displays basic bot info\nping - displays bot brain delay",
+    #  color=0xfff600
+    #)
+   # await ctx.send(embed=embed)
 
   
 async def setup(bot):
